1b09c04d6121e1084b93e5f16ca8626038303775
Некрасов Степан <118102761+S-tepa-n@users.noreply.github.com>
2024-08-27 02:48:36 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+totai_tasks = 12
+hours_speht= 1,5
+training_course= 'Python'
+time_to_solve_1_task =1.5/12
+print('Курс:',training_course,',Всего заданий:',totai_tasks,',Всего затрачено времени',hours_speht,',Среднее время выполнения задачи',time_to_solve_1_task,'часа.')
