c3dcc3a6dd87e9d301f4f95dd386aa003c693c46
Некрасов Степан <118102761+S-tepa-n@users.noreply.github.com>
2024-08-27 13:20:02 +0300

    Update Переменные.py

--- Переменные.py
+++ Переменные.py
@@ -1,5 +1,5 @@
 totai_tasks = 12
 hours_speht= 1,5
 training_course= 'Python'
-time_to_solve_1_task =1.5/12
+time_to_solve_1_task = hours_speht/totai_tasks
 print('Курс:',training_course,',Всего заданий:',totai_tasks,',Всего затрачено времени',hours_speht,',Среднее время выполнения задачи',time_to_solve_1_task,'часа.')
